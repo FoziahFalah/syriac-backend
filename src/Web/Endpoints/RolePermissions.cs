a2573eaed6eda8db3a5aed36aa5027d91677b3db
Merna Geyoushi <merna@darah.org.sa>
2025-03-23 11:51:26 +0300

    Done with permissions :)

--- src/Web/Endpoints/RolePermissions.cs
+++ src/Web/Endpoints/RolePermissions.cs
@@ -3,18 +3,15 @@ using SyriacSources.Backend.Domain.Constants;
 
 namespace SyriacSources.Backend.Web.Endpoints;
 
-
-[Authorize]
 public class RolePermissions: EndpointGroupBase
 {
     public override void Map(WebApplication app)
     {
         app.MapGroup(this)
-            .RequireAuthorization()
-            .MapPut(UpdateRolePermissions, "Update/{roleId}");
+            .MapPut(UpdateRolePermissions, "Update/");
     }
 
-    public async Task<IResult> UpdateRolePermissions(ISender sender, [AsParameters] UpdateRolePermissionCommand command)
+    public async Task<IResult> UpdateRolePermissions(ISender sender, UpdateRolePermissionCommand command)
     {
         await sender.Send(command);
         return Results.NoContent();
